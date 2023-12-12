aa6a353a87bd2846ff11cdfcec3de1e3d7945d97
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2023-12-12 00:30:56 -0300

    updates link router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,10 +7,10 @@ import {
 } from './components/index.pages';
 
 const app_routes: Routes = [
-    {path: '', component: MainComponent},
+    {path: 'home', component: MainComponent},
     {path: 'about', component: AboutComponent},
     {path: 'product', component: ItemdetailComponent},
-    {path: '**', pathMatch: 'full', redirectTo: ''}
+    {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes, {useHash:true});
